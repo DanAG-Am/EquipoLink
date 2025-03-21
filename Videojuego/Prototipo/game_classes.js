614891b3773aa579246c5e8dcf4597bd8b0814b2
jinsik <a01026630@tec.mx>
2025-03-21 15:10:31 -0600

    Updated prototipo

--- Videojuego/Prototipo/game_classes.js
+++ Videojuego/Prototipo/game_classes.js
@@ -87,6 +87,45 @@ class GameObject {
     }
 }
 
+class AnimatedObject extends GameObject {
+    constructor(color, width, height, x, y, type) {
+        super(color, width, height, x, y, type);
+        // Animation properties
+        this.frame = 0;
+        this.minFrame = 0;
+        this.maxFrame = 0;
+        this.sheetCols = 0;
+
+        this.repeat = true;
+
+        // Delay between frames (in milliseconds)
+        this.frameDuration = 100;
+        this.totalTime = 0;
+    }
+
+    setAnimation(minFrame, maxFrame, repeat, duration) {
+        this.minFrame = minFrame;
+        this.maxFrame = maxFrame;
+        this.frame = minFrame;
+        this.repeat = repeat;
+        this.totalTime = 0;
+        this.frameDuration = duration;
+    }
+
+    updateFrame(deltaTime) {
+        this.totalTime += deltaTime;
+        if (this.totalTime > this.frameDuration) {
+            // Loop around the animation frames if the animation is set to repeat
+            // Otherwise stay on the last frame
+            let restartFrame = (this.repeat ? this.minFrame : this.frame);
+            this.frame = this.frame < this.maxFrame ? this.frame + 1 : restartFrame;
+            this.spriteRect.x = this.frame % this.sheetCols;
+            this.spriteRect.y = Math.floor(this.frame / this.sheetCols);
+            this.totalTime = 0;
+        }
+    }
+}
+
 class TextLabel {
     constructor(x, y, font, color) {
         this.x = x;
@@ -104,8 +143,8 @@ class TextLabel {
 
 // Detect a collision of two box objects
 function boxOverlap(obj1, obj2) {
-    return obj1.position.x + obj1.width > obj2.position.x &&
-           obj1.position.x < obj2.position.x + obj2.width &&
-           obj1.position.y + obj1.height > obj2.position.y &&
-           obj1.position.y < obj2.position.y + obj2.height;
+    return obj1.position.x < obj2.position.x + obj2.width &&
+           obj1.position.x + obj1.width > obj2.position.x &&
+           obj1.position.y < obj2.position.y + obj2.height &&
+           obj1.position.y + obj1.height > obj2.position.y
 }
